5fba32d6f35cd8ad0909655293b44281ab61e8d7
upooutofcontext <66369332+upooutofcontext@users.noreply.github.com>
2021-09-17 17:33:33 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+consumer_key = "mXzLJ779Al4UPyg3OJr1NzXdg"
+consumer_secret = "yxQYDNvUs0JNEtEasA9CN4bEweImDIrISQit6eNWEmuCGP008G"
+access_token = "1431634234783981568-WllRiFW0dQW7PjM2eBqYMIsfpqoJc8"
+access_token_secret = "HahcSPryVc553SYEV6tOKPJkK2TcS2RX75jK3oOVNjRY1"
+trigger = ""
+timezone = 2 #change this into your timezone. for example if your timezone is GMT+7 you put 7
